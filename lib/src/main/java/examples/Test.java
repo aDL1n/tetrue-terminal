1186b13c39f505b123b39b9e7cd0b6f982b125f2
BFUR64 <iterranceevan@gmail.com>
2026-06-14 17:34:42 +0800

    refactor: Made `Test.java` in `examples` v3

--- lib/src/main/java/examples/Test.java
+++ lib/src/main/java/examples/Test.java
@@ -1,31 +1,40 @@
 package examples;
 
-import io.github.bfur64.terminal.BufferedTerminal;
 import io.github.bfur64.terminal.input.KeyStroke;
 import io.github.bfur64.terminal.input.KeyType;
-import io.github.bfur64.terminal.interfaces.TerminalBackend;
+import io.github.bfur64.terminal.v3.Terminal;
+import io.github.bfur64.terminal.v3.commands.Command;
+import io.github.bfur64.terminal.v3.interfaces.TerminalRuntime;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-        try (TerminalBackend terminal = BufferedTerminal.auto()) {
-            terminal.start();
+        List<Command> buffer = new ArrayList<>();
+
+        try (TerminalRuntime runtime = Terminal.builder().auto().build()) {
+            Terminal terminal = runtime.terminal();
 
             KeyStroke keyStroke = new KeyStroke('t');
 
             do {
-                terminal.clearScreen();
-                terminal.put(0, 0, "Current Renderer: " + terminal.getTerminalInfo());
+                terminal.clear();
+                terminal.put(0, 0, "Current Renderer: " + terminal.terminalInfo());
 
                 terminal.put(0, 2, "Character: " + keyStroke);
+                buffer.addAll(terminal.snapshotBuffer());
                 terminal.flush();
 
                 if (keyStroke.keyType() == KeyType.ESCAPE) {
                     break;
                 }
 
-                keyStroke = terminal.readInput();
+                keyStroke = terminal.read();
             }
             while (true);
         }
+
+        System.out.println(buffer);
     }
 }
